9b6217975506c2e8502cdacae4313362b7f58384
Stephane de Labrusse <stephdl@de-labrusse.fr>
2017-06-23 22:36:33 +0200

    * Fri Jun 23 2017 stephane de Labrusse <stephdl@de-labrusse.fr> 0.0.2-1.ns6 - initial

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_YumCron.php
@@ -0,0 +1,13 @@
+<?php
+$L['valid_mailforward_address'] = '"${0}" is not a valid email address';
+$L['status_label'] = 'Enable the service';
+$L['checkOnly_label'] = 'Check only or install updates';
+$L['customMail_label'] = 'Add custom recipients for notifications';
+$L['YumCron_header'] = 'Settings of yum-cron';
+$L['YumCron_Title'] = 'Automatic updates';
+$L['Check_Only'] = 'Check updates';
+$L['Automatic_Update'] = 'Install updates';
+$L['customParam_label'] = 'Exclude packages only from yum-cron';
+$L['Download_Only'] = 'Download updates';
+$L['yumAction_label'] = 'Yum action to perform';
+$L['UseWildCard'] = 'You can exclude rpm with *';
---
+++ root/usr/share/nethesis/NethServer/Template/YumCron.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $view Nethgui\Renderer\Xhtml */
+echo $view->header()->setAttribute('template', $T('YumCron_header'));
+
+echo $view->panel()
+->insert($view->fieldsetSwitch('status', 'enabled', $view::FIELDSETSWITCH_CHECKBOX | $view::FIELDSETSWITCH_EXPANDABLE)->setAttribute('uncheckedValue', 'disabled')
+    ->insert($view->textArea('customParam', $view::LABEL_ABOVE)->setAttribute('dimensions', '5x30'))
+    ->insert($view->literal('<div class="labeled-control generated-url-title">'.$T('UseWildCard').'</div>'))
+    ->insert($view->selector('yumAction', $view::SELECTOR_DROPDOWN))
+    ->insert($view->textArea('customMail', $view::LABEL_ABOVE)->setAttribute('dimensions', '5x30'))
+);
+
+echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
