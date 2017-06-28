a269b69bcc602a7a546cac35cd390034d8155f41
Stephane de Labrusse <stephdl@de-labrusse.fr>
2017-06-28 17:53:56 +0200

    moved upgrade dropdown box

--- root/usr/share/nethesis/NethServer/Template/YumCron.php
+++ root/usr/share/nethesis/NethServer/Template/YumCron.php
@@ -4,10 +4,10 @@ echo $view->header()->setAttribute('template', $T('YumCron_header'));
 
 echo $view->panel()
 ->insert($view->literal('<div class="labeled-control generated-url-title">'.$T('WhenUpdatesAreAvailable').'</div>'))
+->insert($view->selector('command', $view::SELECTOR_DROPDOWN))
 ->insert($view->fieldsetSwitch('download', 'yes', $view::FIELDSETSWITCH_CHECKBOX | $view::FIELDSETSWITCH_EXPANDABLE)
     ->setAttribute('uncheckedValue', 'no')
-    ->insert($view->checkBox('applyUpdate', 'yes')->setAttribute('uncheckedValue', 'no'))
-    ->insert($view->selector('command', $view::SELECTOR_DROPDOWN)))
+    ->insert($view->checkBox('applyUpdate', 'yes')->setAttribute('uncheckedValue', 'no')))
 
 ->insert($view->fieldsetSwitch('messages', 'yes', $view::FIELDSETSWITCH_CHECKBOX | $view::FIELDSETSWITCH_EXPANDABLE)
     ->setAttribute('uncheckedValue', 'no')
