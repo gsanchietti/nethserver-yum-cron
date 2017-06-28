74b389f7a4f8bdeb47b6ea19898a8bbbb8b61c69
Stephane de Labrusse <stephdl@de-labrusse.fr>
2017-06-28 17:55:17 +0200

    moved upgrade dropdown box

--- root/usr/share/nethesis/NethServer/Template/YumCron.php
+++ root/usr/share/nethesis/NethServer/Template/YumCron.php
@@ -4,7 +4,6 @@ echo $view->header()->setAttribute('template', $T('YumCron_header'));
 
 echo $view->panel()
 ->insert($view->literal('<div class="labeled-control generated-url-title">'.$T('WhenUpdatesAreAvailable').'</div>'))
-->insert($view->selector('command', $view::SELECTOR_DROPDOWN))
 ->insert($view->fieldsetSwitch('download', 'yes', $view::FIELDSETSWITCH_CHECKBOX | $view::FIELDSETSWITCH_EXPANDABLE)
     ->setAttribute('uncheckedValue', 'no')
     ->insert($view->checkBox('applyUpdate', 'yes')->setAttribute('uncheckedValue', 'no')))
@@ -12,6 +11,7 @@ echo $view->panel()
 ->insert($view->fieldsetSwitch('messages', 'yes', $view::FIELDSETSWITCH_CHECKBOX | $view::FIELDSETSWITCH_EXPANDABLE)
     ->setAttribute('uncheckedValue', 'no')
     ->insert($view->textArea('customMail', $view::LABEL_ABOVE)->setAttribute('dimensions', '5x30')))
+->insert($view->selector('command', $view::SELECTOR_DROPDOWN))
 ;
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
